ede279411cc80cd7c9ebaf50f740e06cbe16ccb3
CITADEL\mvasq <mvasq95@hotmail.com>
2023-08-15 04:15:01 -0400

    removed used of dotnetenv, used the C# approach of using configuration instead.

--- Data/Services/TweetService.cs
+++ Data/Services/TweetService.cs
@@ -1,34 +1,37 @@
 using System;
 using System.Collections.Generic;
-using System.Linq;
 using System.Threading.Tasks;
-using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Configuration;
 using RestSharp;
 using RestSharp.Authenticators;
-using DotNetEnv;
 
 namespace Tweets.Data
 {
     public class TweetService : ITweetService
     {
+        private readonly IConfiguration _configuration;
+
+        public TweetService(IConfiguration configuration)
+        {
+            _configuration = configuration;
+        }
+
         public async Task<List<Tweet>> GetTenUserQueriedTweets(string user_query)
         {
             RestClient client = new RestClient("https://api.twitter.com/2/tweets");
 
             RestRequest request = new RestRequest($"/search/recent?query={user_query}&tweet.fields=public_metrics,created_at&expansions=author_id,attachments.media_keys&media.fields=url&user.fields=name,profile_image_url", DataFormat.Json);
 
-            DotNetEnv.Env.Load();
-
-            string bearerToken = Env.GetString("BEARER_TOKEN");
+            string bearerToken = _configuration["TwitterBearerToken"];
 
             request.AddParameter("Authorization", string.Format("Bearer " + bearerToken), ParameterType.HttpHeader);
 
             var response = await client.GetAsync<TwitterResponse>(request);
 
-            if (response.includes != null && response.includes.Count > 0 && response.includes[0].users != null)
+            if (response.includes != null && response.includes.Count > 0 && response.includes[0].users != null && response.data != null)
             {
                 int loopCount = Math.Min(response.includes[0].users.Count, 10);
-                for (int i = 0; i < loopCount; i++)
+                for (int i = 0; i < loopCount && i < response.data.Count; i++)
                 {
                     response.data[i].name = response.includes[0].users[i].name;
                     response.data[i].profile_image_url = response.includes[0].users[i].profile_image_url;
@@ -45,9 +48,7 @@ namespace Tweets.Data
 
             RestRequest request = new RestRequest($"/{author_id}/tweets?expansions=author_id&tweet.fields=public_metrics,created_at&user.fields=profile_image_url", DataFormat.Json);
 
-            DotNetEnv.Env.Load();
-
-            string bearerToken = Env.GetString("BEARER_TOKEN");
+            string bearerToken = _configuration["TwitterBearerToken"];
 
             request.AddParameter("Authorization", string.Format("Bearer " + bearerToken), ParameterType.HttpHeader);
 
